a7a91b8522346bb7e2bb786709a5ed5f2066ef54
andreipuscas <andreipuscas2004@gmail.com>
2024-10-16 00:14:20 +0300

    Task 1, tot ce era de facut

--- lab1/src/task1.java
+++ lab1/src/task1.java
@@ -19,4 +19,36 @@ public class task1 {
         double average = (double) sum / grades.length;
         System.out.println("Average grade: " + average);
     }
+
+    public static void roundGrades(int[] grades) {
+        System.out.print("Round grades: ");
+        for (int grade : grades) {
+            if (grade >= 38 && (5 - (grade % 5)) < 3) {
+                System.out.print(grade + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void findMaxRoundedGrade(int[] grades) {
+        int maxGrade = 0;
+        for (int grade : grades) {
+            if (grade >= 38 && (5 - (grade % 5)) < 3) {
+                grade += 5 - (grade % 5);
+            }
+            if (grade > maxGrade) {
+                maxGrade = grade;
+            }
+        }
+        System.out.println("Max rounded grade: " + maxGrade);
+    }
+
+    public static void main(String[] args) {
+        int[] grades = {29, 37, 38, 41, 84, 67};
+
+        findFailingGrades(grades);
+        calculateAverage(grades);
+        roundGrades(grades);
+        findMaxRoundedGrade(grades);
+    }
 }
