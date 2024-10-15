44372cf1abc5029379f207a0418dddb6e2d36ef7
andreipuscas <andreipuscas2004@gmail.com>
2024-10-16 00:23:37 +0300

    task 4, tot

--- lab1/src/task4.java
+++ lab1/src/task4.java
@@ -23,4 +23,26 @@ public class task4 {
         }
         System.out.println("Most Expensive Item: " + max);
     }
+
+    public static void findMaxSpend(int[] keyboards, int[] usbs, int budget) {
+        int maxSpend = -1;
+        for (int k : keyboards) {
+            for (int u : usbs) {
+                if (k + u <= budget && k + u > maxSpend) {
+                    maxSpend = k + u;
+                }
+            }
+        }
+        System.out.println("Max Spend: " + maxSpend);
+    }
+
+    public static void main(String[] args) {
+        int[] keyboards = {40, 50, 60};
+        int[] usbs = {8, 12};
+        int budget = 60;
+
+        findCheapestKeyboard(keyboards);
+        findMostExpensiveItem(keyboards, usbs);
+        findMaxSpend(keyboards, usbs, budget);
+    }
 }
